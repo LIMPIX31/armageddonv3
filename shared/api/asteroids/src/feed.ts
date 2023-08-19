b8664f807c801e287f60f539617cd05804a642d3
Danil Karpenko <limpix31@gmail.com>
2023-08-19 15:40:19 +0200

    fix: sorting and filters

--- shared/api/asteroids/src/feed.ts
+++ shared/api/asteroids/src/feed.ts
@@ -14,11 +14,16 @@ export async function feed({ date }: BrowseParams): Promise<FeedResponse> {
 
 	return fetch(url)
 		.then((res) => res.json())
-		.then((data) => ({
+		.then((data: FeedResponse) => ({
 			...data,
 			near_earth_objects: Object.entries(data.near_earth_objects)
 				.sort(([a], [b]) => new Date(a).getTime() - new Date(b).getTime())
-				.flatMap(([, value]) => value),
+				.flatMap(([, value]) => value)
+				.sort(
+					(a, b) =>
+						a.close_approach_data[0]!.epoch_date_close_approach - b.close_approach_data[0]!.epoch_date_close_approach,
+				)
+				.filter(({ close_approach_data }) => close_approach_data[0]!.epoch_date_close_approach > Date.now()),
 			pagination: {
 				prev: new Date(new Date(date).getTime() - WEEK).toISOString().slice(0, 10),
 				next: new Date(new Date(date).getTime() + WEEK).toISOString().slice(0, 10),
